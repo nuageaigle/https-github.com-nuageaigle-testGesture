0843716cb752cf4215af8f74c9d0bf642606f347
nuage <nuageaigle@gmail.com>
2020-09-09 20:35:13 +0800

    Initial Commit

---
+++ testGesture2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testGesture2
+//
+//  Created by aigle nuage on 2020/9/9.
+//  Copyright © 2020 aigle nuage. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
