ef321317cfcda445e7b35d9db3af2ea573f5e2a7
nuage <nuageaigle@gmail.com>
2020-09-25 22:35:24 +0800

    Stack align by "frame( alignment: )"

--- StackAlignment.swift
+++ StackAlignment.swift
@@ -27,21 +27,31 @@ struct GeometryToVarStackSlign: View {
     
     var body: some View {
 
-            GeometryReader { geo in
+        GeometryReader { geo in
                 ZStack(alignment: .center) {
-                    getSize(metrics: geo)
-                    Rectangle()
-                        .fill(Color.clear)
+                    // ZStack has a invisible frame, this frame size by child views,
                     
-                    Text(" \(mmmSize.width) x \(mmmSize.height) \n \(gW)")
-                    Text("top leading").frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 200, alignment: .topLeading)
+                    getSize(metrics: geo)
+                    /*
+                    Rectangle()
+                        .fill(Color.blue.opacity(0.3))
+                        .frame(width: geo.size.width / 2, height: geo.size.height / 2)*/
+                    
+                    Text("w \(mmmSize.width)\nh \(mmmSize.height) \n \(gW)")
+                    Text("top leading")
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 200, alignment: .topLeading)
                     // Text alignt to it self fram
                     // and frame alignt on .center
                     
-                    Text("bottom traling").frame(width: geo.size.width, height: geo.size.height, alignment: .bottomTrailing)
+                    Text("bottom traling")
+                        .frame(width: geo.size.width / 2, height: geo.size.height / 2, alignment: .bottomTrailing)
                     // Text alignt to it self fram
                     
                 }
+                .frame(width: geo.size.width * 0.8, height: geo.size.height * 0.8, alignment: .bottomTrailing)
+                // ZStack's visible frame,
+                
+                
             }
         
     }
